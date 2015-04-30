d72b7502021e92720f51a480f778c16b82da6e9b
Josh Juster <juster@gmail.com>
2015-04-30 12:02:08 -0400

    try another `

--- wp-config.php
+++ wp-config.php
@@ -36,8 +36,8 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define('WP_SITEURL', 'http://pradux-news.herokuapp.com');
-//define('WP_HOME', 'http://pradux-next.herokuapp.com/news');
+define('WP_SITEURL', 'http://pradux-news.herokuapp.com/news');
+define('WP_HOME', 'http://pradux-next.herokuapp.com/news');
 
 /**#@+
  * Authentication Unique Keys and Salts.

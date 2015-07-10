1a6cc0bee4e21e9b853a5d74703547bada3a6eb7
Adam Bailin <abailin@gmail.com>
2015-07-10 18:48:46 +0200

    editing theme

--- wp-content/themes/twentyfifteen/content-page.php
+++ wp-content/themes/twentyfifteen/content-page.php
@@ -16,7 +16,7 @@
 
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<h3><?php get_the_category(); ?> | <?php the_date(); ?></h3>
+		<h3><?php the_category(', '); ?> | <?php the_date(); ?></h3>
 	</header>
 
 	<div class="entry-content">

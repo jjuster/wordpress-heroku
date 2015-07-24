6f2cc898d7b7c910db0075f136914c53b0085491
Adam Bailin <abailin@gmail.com>
2015-07-24 11:57:18 +0200

    related post zoom fix

--- wp-content/themes/twentyfifteen/content.php
+++ wp-content/themes/twentyfifteen/content.php
@@ -137,15 +137,15 @@ $nextID = $posts[$current+1];
 			<?php foreach ($related_posts as $related_post): ?>
 			<a href="<?=$related_post->permalink?>" class="related-post">
 				<span style="background-image:url(<?=$related_post->featured_image?>)">
-					<div class="bottom-text">
-						<div class="category">
-							<?=htmlencode($related_post->category)?>
-						</div>
-						<div class="title">
-							<?=htmlencode($related_post->post_title)?>
-						</div>
-					</div>
 				</span>
+				<div class="bottom-text">
+					<div class="category">
+						<?=htmlencode($related_post->category)?>
+					</div>
+					<div class="title">
+						<?=htmlencode($related_post->post_title)?>
+					</div>
+				</div>
 			</a>
 			<?php endforeach; ?>
 

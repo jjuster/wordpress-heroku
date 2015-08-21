5fdc144b14ebcbe23f3df77c85b5daab37ef56cb
Adam Bailin <abailin@gmail.com>
2015-08-21 14:41:28 -0400

    homepage

--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -14,40 +14,30 @@
  * @since Twenty Fifteen 1.0
  */
 
- ?>
+$posts = new WP_Query('posts_per_page=10');
+$post_ids = array();
+$post_debug = array();
+
+get_header(); ?>
 
 	<!-- [index] -->
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php if ( $posts->have_posts() ) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+			
 
 			<?php
 			// Start the loop.
-			while ( have_posts() ) : the_post();
+			while ( $posts->have_posts() ) : $posts->the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
+				$post_ids[] = $post->ID;
+				$post_debug[] = $post;
 
-			// End the loop.
 			endwhile;
 
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-			) );
+
 
 		// If no content, include the "No posts found" template.
 		else :
@@ -58,5 +48,10 @@
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
+
+<script>
+	var post_ids = <?=json_encode($post_ids)?>;
+	var post_debug = <?=json_encode($post_debug)?>;
+</script>
 
 <?php get_footer(); ?>

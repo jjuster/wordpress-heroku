7eefd0a04fc531490737082e2ea23b36ddff8bcc
Adam Bailin <abailin@gmail.com>
2015-08-26 12:47:52 -0400

    masonry

--- wp-content/themes/twentyfifteen/index.php
+++ wp-content/themes/twentyfifteen/index.php
@@ -263,13 +263,13 @@ function load_more()
 			setTimeout(function() {
 				$(".homepage-post").removeClass("masonry-new");
 				currently_loading_more = false;
-			}, 50);
+			}, 1000);
 
 		}
 	});
 }
 
-var load_more_debounced = _.debounce(load_more, 500);
+var load_more_debounced = _.debounce(load_more, 1000);
 $(window).scroll(load_more_debounced);
 
 </script>
